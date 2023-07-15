d7fdab71e1c6ff37eaa7c9a2fcf616c43148268c
azisdev <aziz.mcgr@gmail.com>
2023-07-15 10:35:13 +0700

    forms LOV

---
+++ project3.lpr
@@ -0,0 +1,27 @@
+program project3;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, unit3, unlov, ULov
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormLOV, FormLOV);
+  Application.CreateForm(TFrmLov, FrmLov);
+  Application.Run;
+end.
+
---
+++ unit3.pas
@@ -0,0 +1,89 @@
+unit unit3;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, DBGrids,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure HandleLOVResult(const Value: string);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+uses
+  unlov,ULov;
+
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  FormLOV: TFormLOV;
+  ButtonPos: TPoint;
+begin
+  ButtonPos := Button3.ClientToScreen(Point(0, 0));
+  FormLOV := TFormLOV.Create(Self);
+
+  try
+    FormLOV.Left := ButtonPos.X;
+    FormLOV.Top :=  ButtonPos.Y + Button3.Height;
+    FormLOV.ShowModal;
+  finally
+    FormLOV.Free;
+  end;
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  //FormLOV: TFormLOV;
+  FrmLov:TFrmLov;
+
+  ButtonPos: TPoint;
+begin
+  ButtonPos := Button1.ClientToScreen(Point(0, 0));
+  FrmLov := TFrmLov.Create(Self);
+
+  try
+    FrmLov.Left := ButtonPos.X;
+    FrmLov.Top :=  ButtonPos.Y + Button3.Height;
+    FrmLov.ShowModal;
+  finally
+    FrmLov.Free;
+  end;
+
+
+end;
+
+procedure TForm1.HandleLOVResult(const Value: string);
+begin
+  // Disini Anda dapat menggunakan nilai yang dipilih dari LOV
+  ShowMessage('Nilai yang dipilih: ' + Value);
+end;
+
+{ TForm1 }
+
+
+end.
+
